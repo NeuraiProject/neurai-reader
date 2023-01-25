123bcae30f51dbeace0e33af10012777d0a2aa11
Dick Larsson <dicklarsson@gmail.com>
2023-01-25 12:04:29 +0100

    Adding TypesScript type definition file

---
+++ dist/types.d.ts
@@ -0,0 +1,27 @@
+declare function setURL(newURL: string): void;
+declare function setUsername(newUsername: string): void;
+declare function setPassword(newPassword: string): void;
+declare function getAddressDeltas(addresses: Array<string>): Promise<any[]>;
+declare function getAssetBalance(addresses: Array<string>): Promise<any>;
+declare function getAsset(name: string): Promise<any>;
+declare function getAllAssets(prefix?: string, includeAllMetaData?: boolean): Promise<any>;
+declare function getMempool(): Promise<any>;
+declare function verifyMessage(address: string, signature: string, message: string): Promise<boolean>;
+declare function getRavencoinBalance(addresses: Array<string>): {};
+declare const _default: {
+    getAddressDeltas: typeof getAddressDeltas;
+    getAllAssets: typeof getAllAssets;
+    getAsset: typeof getAsset;
+    getAssetBalance: typeof getAssetBalance;
+    getMempool: typeof getMempool;
+    getRavencoinBalance: typeof getRavencoinBalance;
+    setUsername: typeof setUsername;
+    setPassword: typeof setPassword;
+    setURL: typeof setURL;
+    verifyMessage: typeof verifyMessage;
+    URL_MAINNET: string;
+    URL_TESTNET: string;
+};
+export default _default;
+
+//# sourceMappingURL=types.d.ts.map
